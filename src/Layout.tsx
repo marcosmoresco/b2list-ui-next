9987c6404edb4841447a4c147e734fc029b55e78
Marcos Roberto Moresco <marcoshoppusmoresco@gmail.com>
2022-03-03 14:36:21 -0300

    foo

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { styled } from "@b2list/config/stitches.config";
 
-const Box = styled('div', {})
+const Box = styled('div', {});
 
 export type LayoutProps = {
   children: React.ReactNode;
